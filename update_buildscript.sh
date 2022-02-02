571a86cc05e677ccf2a1834abfe696b96ca9615c
bombcar <github@bombcar.com>
2022-02-02 16:11:05 -0600

    fix gradlew also

--- update_buildscript.sh
+++ update_buildscript.sh
@@ -2,4 +2,4 @@
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/build.gradle -o build.gradle -sS && echo "Build.gradle downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/release-tags.yml -o .github/workflows/release-tags.yml -sS --create-dirs && echo "Release Tags action downloaded" 
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/CODEOWNERS -o CODEOWNERS -sS && echo "CODEOWNERS downloaded"
-git add CODEOWNERS .github/workflows/release-tags.yml build.gradle && echo "Everything added to git"
+git update-index --chmod=+x gradlew && git add CODEOWNERS .github/workflows/release-tags.yml build.gradle && echo "Everything added to git"
