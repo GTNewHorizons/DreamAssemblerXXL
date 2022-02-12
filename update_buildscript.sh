da46f10c57d636cf2fafa6f23434fc03faf93236
bombcar <github@bombcar.com>
2022-02-12 08:13:43 -0600

    use update-buildscript as a generic fixer

--- update_buildscript.sh
+++ update_buildscript.sh
@@ -2,4 +2,5 @@
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/build.gradle -o build.gradle -sS && echo "Build.gradle downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/release-tags.yml -o .github/workflows/release-tags.yml -sS --create-dirs && echo "Release Tags action downloaded" 
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/CODEOWNERS -o CODEOWNERS -sS && echo "CODEOWNERS downloaded"
-git update-index --chmod=+x gradlew && git add CODEOWNERS .github/workflows/release-tags.yml build.gradle && echo "Everything added to git"
+git update-index --chmod=+x .github/scripts/test-no-crash-reports.sh && chmod +x .github/scripts/test-no-crash-reports.sh
+git update-index --chmod=+x gradlew && chmod +x gradlew && git add CODEOWNERS .github/workflows/release-tags.yml build.gradle && echo "Everything added to git"
