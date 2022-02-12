b5c65841af8c5d8f40757a20df89b66f1364f634
bombcar <github@bombcar.com>
2022-02-12 09:37:22 -0600

    chmod after add

--- update_buildscript.sh
+++ update_buildscript.sh
@@ -5,5 +5,7 @@ curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/CODEO
 rm -f .github/scripts/test-no-crash-reports.sh
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/scripts/test-no-error-reports.sh -o .github/scripts/test-no-error-reports.sh -sS && echo "Test Errors script downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/build-and-test.yml -o .github/workflows/build-and-test.yml -sS && echo "Build and Test action downloaded"
+
+git add CODEOWNERS .github/scripts/test-no-error-reports.sh .github/workflows/release-tags.yml .github/workflows/build-and-test.yml build.gradle && echo "Everything added to git"
 git update-index --chmod=+x .github/scripts/test-no-error-reports.sh && chmod +x .github/scripts/test-no-error-reports.sh
-git update-index --chmod=+x gradlew && chmod +x gradlew && git add CODEOWNERS .github/scripts/test-no-error-reports.sh .github/workflows/release-tags.yml .github/workflows/build-and-test.yml build.gradle && echo "Everything added to git"
+git update-index --chmod=+x gradlew && chmod +x gradlew
