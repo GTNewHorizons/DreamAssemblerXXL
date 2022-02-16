c698c2c44080487eb9924bbcae2d52e519c987b7
bombcar <github@bombcar.com>
2022-02-16 14:52:18 -0600

    more vers (#23)
    * more vers
    
    * sides
    
    * read stuff forgotten
    
    * add to git
    
    * CTMlib is merged
    
    * update
    
    * oauth
    
    * more mods

--- update_buildscript.sh
+++ update_buildscript.sh
@@ -5,7 +5,8 @@ curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/CODEO
 rm -f .github/scripts/test-no-crash-reports.sh
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/scripts/test-no-error-reports.sh -o .github/scripts/test-no-error-reports.sh -sS && echo "Test Errors script downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/build-and-test.yml -o .github/workflows/build-and-test.yml -sS && echo "Build and Test action downloaded"
+curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.editorconfig -o .editorconfig -sS && echo "editorconfig downloaded"
 
-git add CODEOWNERS .github/scripts/test-no-error-reports.sh .github/workflows/release-tags.yml .github/workflows/build-and-test.yml build.gradle && echo "Everything added to git"
+git add CODEOWNERS .github/scripts/test-no-error-reports.sh .github/workflows/release-tags.yml .github/workflows/build-and-test.yml .editorconfig build.gradle && echo "Everything added to git"
 git update-index --chmod=+x .github/scripts/test-no-error-reports.sh && chmod +x .github/scripts/test-no-error-reports.sh
 git update-index --chmod=+x gradlew && chmod +x gradlew
