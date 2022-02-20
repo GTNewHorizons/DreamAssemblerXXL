d6066d299e183ad972f354f4ef90df81e6e242bd
bombcar <github@bombcar.com>
2022-02-20 16:17:16 -0600

    update to reflect examplemod changes

--- update_buildscript.sh
+++ update_buildscript.sh
@@ -2,8 +2,8 @@
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/build.gradle -o build.gradle -sS && echo "Build.gradle downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/release-tags.yml -o .github/workflows/release-tags.yml -sS --create-dirs && echo "Release Tags action downloaded" 
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/CODEOWNERS -o CODEOWNERS -sS && echo "CODEOWNERS downloaded"
-rm -f .github/scripts/test-no-crash-reports.sh
-curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/scripts/test-no-error-reports.sh -o .github/scripts/test-no-error-reports.sh -sS && echo "Test Errors script downloaded"
+rm -f .github/scripts/test-no-*
+curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/scripts/test_no_error_reports -o .github/scripts/test_no_error_reports -sS && echo "Test Errors script downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/build-and-test.yml -o .github/workflows/build-and-test.yml -sS && echo "Build and Test action downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.editorconfig -o .editorconfig -sS && echo "editorconfig downloaded"
 
