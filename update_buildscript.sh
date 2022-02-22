214f9f1e998ff2ac8c270cb93db1d6620425727d
bombcar <github@bombcar.com>
2022-02-22 10:56:25 -0600

    update to reflect examplemod changes (#24)
    * update to reflect examplemod changes
    
    * chmood

--- update_buildscript.sh
+++ update_buildscript.sh
@@ -2,11 +2,11 @@
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/build.gradle -o build.gradle -sS && echo "Build.gradle downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/release-tags.yml -o .github/workflows/release-tags.yml -sS --create-dirs && echo "Release Tags action downloaded" 
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/CODEOWNERS -o CODEOWNERS -sS && echo "CODEOWNERS downloaded"
-rm -f .github/scripts/test-no-crash-reports.sh
-curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/scripts/test-no-error-reports.sh -o .github/scripts/test-no-error-reports.sh -sS && echo "Test Errors script downloaded"
+rm -f .github/scripts/test-no-*
+curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/scripts/test_no_error_reports -o .github/scripts/test_no_error_reports -sS && echo "Test Errors script downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.github/workflows/build-and-test.yml -o .github/workflows/build-and-test.yml -sS && echo "Build and Test action downloaded"
 curl https://raw.githubusercontent.com/GTNewHorizons/ExampleMod1.7.10/main/.editorconfig -o .editorconfig -sS && echo "editorconfig downloaded"
 
-git add CODEOWNERS .github/scripts/test-no-error-reports.sh .github/workflows/release-tags.yml .github/workflows/build-and-test.yml .editorconfig build.gradle && echo "Everything added to git"
-git update-index --chmod=+x .github/scripts/test-no-error-reports.sh && chmod +x .github/scripts/test-no-error-reports.sh
+git add CODEOWNERS .github/scripts/test_no_error_reports .github/workflows/release-tags.yml .github/workflows/build-and-test.yml .editorconfig build.gradle && echo "Everything added to git"
+git update-index --chmod=+x .github/scripts/test_no_error_reports && chmod +x .github/scripts/test_no_error_reports
 git update-index --chmod=+x gradlew && chmod +x gradlew
