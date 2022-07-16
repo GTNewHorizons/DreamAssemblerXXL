bee209914a8859c5ffc0acae66b21fe7cf9302b4
Jason Mitchell <mitchej@gmail.com>
2022-07-16 10:12:55 -0700

    Handle no test-no-crash-report.sh

--- scripts/migate_github_actions.sh
+++ scripts/migate_github_actions.sh
@@ -9,14 +9,18 @@ fi
 echo "Removing existing workflows if they exist"
 [ ! -e .github/workflows/build-and-test.yml ] || rm .github/workflows/build-and-test.yml
 [ ! -e .github/workflows/release-tags.yml ] || rm .github/workflows/release-tags.yml
-[ ! -e .github/scripts/test-no-crash-reports.sh ] || rm .github/scripts/test-no-crash-reports.sh
+
+if [ -e .github/scripts/test-no-crash-reports.sh ]; then
+  rm .github/scripts/test-no-crash-reports.sh
+  git add .github/scripts/test-no-crash-reports.sh
+fi
 
 echo "Copying new workflows from ExampleMod1.7.10"
 cp ~/dev/mc/ExampleMod1.7.10/.github/workflows/build-and-test.yml .github/workflows/
 cp ~/dev/mc/ExampleMod1.7.10/.github/workflows/release-tags.yml .github/workflows/
 
 echo "Adding files and committing"
-git add .github/scripts/test-no-crash-reports.sh .github/workflows/build-and-test.yml .github/workflows/release-tags.yml
-git commit -m "Migrate github actions to GTNH-Actions-Workflows"
+git add .github/workflows/build-and-test.yml .github/workflows/release-tags.yml
+git commit -m "[ci skip] Migrate github actions to GTNH-Actions-Workflows"
 
 git push origin master
