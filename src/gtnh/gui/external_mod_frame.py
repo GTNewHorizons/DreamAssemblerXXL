8e40d6424531147aa0277d83b8f41601916030aa
boubou_19 <miisterunknown@gmail.com>
2022-08-16 22:49:20 +0200

    reworked external_mod_frame.py

--- src/gtnh/gui/external_mod_frame.py
+++ src/gtnh/gui/external_mod_frame.py
@@ -1,5 +1,5 @@
 from tkinter import Button, LabelFrame, Listbox, Scrollbar, StringVar
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from gtnh.gui.mod_info_frame import ModInfoFrame
 
@@ -7,7 +7,7 @@ from gtnh.gui.mod_info_frame import ModInfoFrame
 class ExternalModList(LabelFrame):
     """Widget handling the list of external mods."""
 
-    def __init__(self, master: Any, frame_name: str, callbacks: Dict[str, Any], **kwargs: Any):
+    def __init__(self, master: Any, frame_name: str, callbacks: Dict[str, Any], width: Optional[int] = None, **kwargs: Any):
         """
         Constructor of the ExternalModList class.
 
@@ -17,8 +17,14 @@ class ExternalModList(LabelFrame):
         :param kwargs: params to init the parent class
         """
         LabelFrame.__init__(self, master, text=frame_name, **kwargs)
-        self.ypadding: int = 20  # todo: tune this
+        self.ypadding: int = 0  # todo: tune this
         self.xpadding: int = 0  # todo: tune this
+
+        self.btn_add_text = "add new"
+        self.btn_rem_text = "delete highlighted"
+
+        self.width = width if width is not None else max(len(self.btn_add_text), len(self.btn_rem_text))
+
         self.sv_repo_name: StringVar = StringVar(self, value="")
 
         self.lb_mods: Listbox = Listbox(self, exportselection=False)
@@ -30,6 +36,55 @@ class ExternalModList(LabelFrame):
         self.lb_mods.configure(yscrollcommand=self.scrollbar.set)
         self.scrollbar.configure(command=self.lb_mods.yview)
 
+    def configure_widgets(self) -> None:
+        """
+        Method to configure the widgets.
+
+        :return: None
+        """
+        self.btn_add.configure(width=self.width)
+        self.btn_rem.configure(width=self.width)
+
+    def set_width(self, width: int) -> None:
+        """
+        Method to set the widgets' width.
+
+        :param width: the new width
+        :return: None
+        """
+        self.width = width
+        self.configure_widgets()
+
+    def get_width(self) -> int:
+        """
+        Getter for self.width.
+
+        :return: the width in character sizes of the normalised widgets
+        """
+        return self.width
+
+    def update_widget(self) -> None:
+        """
+        Method to update the widget and all its childs
+
+        :return: None
+        """
+        self.hide()
+        self.configure_widgets()
+        self.show()
+
+    def hide(self) -> None:
+        """
+        Method to hide the widget and all its childs
+        :return None:
+        """
+        self.lb_mods.grid_forget()
+        self.scrollbar.grid_forget()
+        self.btn_add.grid_forget()
+        self.btn_rem.grid_forget()
+
+        self.update_idletasks()
+
     def show(self) -> None:
         """
         Method used to display widgets and child widgets, as well as to configure the "responsiveness" of the widgets.
@@ -40,14 +95,18 @@ class ExternalModList(LabelFrame):
         y: int = 0
 
         self.columnconfigure(0, weight=1, pad=self.ypadding)
+        self.columnconfigure(1, weight=1, pad=self.ypadding)
+
         self.rowconfigure(0, weight=1, pad=self.xpadding)
         self.rowconfigure(1, weight=1, pad=self.xpadding)
         self.rowconfigure(2, weight=1, pad=self.xpadding)
 
         self.lb_mods.grid(row=x, column=y, columnspan=2, sticky="WE")
-        self.scrollbar.grid(row=x, column=y + 2, columnspan=2, sticky="NS")
-        self.btn_add.grid(row=x + 1, column=y, sticky="WE")
-        self.btn_rem.grid(row=x + 1, column=y + 1, sticky="WE")
+        self.scrollbar.grid(row=x, column=y + 2, sticky="NS")
+        self.btn_add.grid(row=x + 1, column=y)
+        self.btn_rem.grid(row=x + 1, column=y + 1, columnspan=2)
+
+        self.update_idletasks()
 
     def populate_data(self, data: Any) -> None:
         """
@@ -62,7 +121,7 @@ class ExternalModList(LabelFrame):
 class ExternalModFrame(LabelFrame):
     """Main frame widget for the external mods' management."""
 
-    def __init__(self, master: Any, frame_name: str, callbacks: Dict[str, Any], **kwargs: Any):
+    def __init__(self, master: Any, frame_name: str, callbacks: Dict[str, Any], width:Optional[int]=None, **kwargs: Any):
         """
         Constructor of the ExternalModFrame class.
 
@@ -75,6 +134,8 @@ class ExternalModFrame(LabelFrame):
         self.xpadding: int = 0  # todo: tune this
         LabelFrame.__init__(self, master, text=frame_name, **kwargs)
 
+        self.width:Optional[int] = width
+
         mod_info_callbacks: Dict[str, Any] = {
             "set_mod_version": callbacks["set_external_mod_version"],
             "set_mod_side": callbacks["set_external_mod_side"],
@@ -83,6 +144,77 @@ class ExternalModFrame(LabelFrame):
             self, frame_name="external mod info", callbacks=mod_info_callbacks
         )
         self.external_mod_list: ExternalModList = ExternalModList(self, frame_name="external mod list", callbacks={})
+
+        if self.width is None:
+            self.width = self.external_mod_list.get_width()
+            self.mod_info_frame.set_width(width)
+            self.update_widget()
+
+        else:
+            self.mod_info_frame.set_width(self.width)
+            self.external_mod_list.set_width(self.width)
+
+    def configure_widgets(self) -> None:
+        """
+        Method to configure the widgets.
+
+        :return: None
+        """
+        self.mod_info_frame.configure_widgets()
+        self.external_mod_list.configure_widgets()
+
+    def set_width(self, width: int) -> None:
+        """
+        Method to set the widgets' width.
+
+        :param width: the new width
+        :return: None
+        """
+        self.width = width
+        self.mod_info_frame.set_width(self.width)
+        self.external_mod_list.set_width(self.width)
+
+    def get_width(self) -> int:
+        """
+        Getter for self.width.
+
+        :return: the width in character sizes of the normalised widgets
+        """
+        return self.width
+
+    def configure_widgets(self) -> None:
+        """
+        Method to configure the widgets.
+
+        :return: None
+        """
+        pass
+
+    def update_widget(self) -> None:
+        """
+        Method to update the widget and all its childs
+
+        :return: None
+        """
+        self.hide()
+        self.configure_widgets()
+        self.show()
+
+        self.external_mod_list.update_widget()
+        self.mod_info_frame.update_widget()
+
+    def hide(self) -> None:
+        """
+        Method to hide the widget and all its childs
+        :return None:
+        """
+        self.external_mod_list.grid_forget()
+        self.mod_info_frame.grid_forget()
+
+        self.external_mod_list.hide()
+        self.mod_info_frame.hide()
+
+        self.update_idletasks()
 
     def show(self) -> None:
         """
@@ -94,13 +226,13 @@ class ExternalModFrame(LabelFrame):
         self.rowconfigure(0, weight=1, pad=self.xpadding)
         self.rowconfigure(1, weight=1, pad=self.xpadding)
 
-        self.external_mod_list.grid(row=0, column=0, sticky="WE")
-        self.mod_info_frame.grid(row=1, column=0, sticky="WE")
-
-        self.master.update_idletasks()
+        self.external_mod_list.grid(row=0, column=0)
+        self.mod_info_frame.grid(row=1, column=0)
 
         self.external_mod_list.show()
         self.mod_info_frame.show()
+
+        self.update_idletasks()
 
     def populate_data(self, data: Any) -> None:
         """
