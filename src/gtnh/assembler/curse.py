6408adaca9f1c186b8071ae681d35371d4546d7b
boubou_19 <miisterunknown@gmail.com>
2022-08-28 19:11:24 +0200

    base work for cursed assembler

--- src/gtnh/assembler/curse.py
+++ src/gtnh/assembler/curse.py
@@ -1,104 +1,23 @@
-# from typing import Any
-#
-# from httpx import AsyncClient
-# from structlog import get_logger
-#
-# from gtnh.defs import CURSE_BASE_URL, CURSE_FORGE_MODLOADER_ID, CURSE_GAME_VERSION_TYPE_ID, CURSE_MINECRAFT_ID
-# from gtnh.models.mod_info import ExternalModInfo
-# from gtnh.utils import get_curse_token, grouper
-#
-# log = get_logger(__name__)
-#
-#
-# def get_headers() -> dict[str, str]:
-#     return {"Accept": "application/json", "x-api-key": get_curse_token()}
-#
-#
-# async def slug_to_project_id(slug: str, client: AsyncClient) -> str:
-#     response = await client.get(
-#         url=CURSE_BASE_URL + "/v1/mods/search",
-#         headers=get_headers(),
-#         params={
-#             "gameId": CURSE_MINECRAFT_ID,
-#             "gameVersionTypeId": CURSE_GAME_VERSION_TYPE_ID,
-#             "modLoaderType": CURSE_FORGE_MODLOADER_ID,
-#             "slug": slug,
-#         },
-#     )
-#     if response.status_code != 200:
-#         log.error(f"Cannot find mod by slug {slug}!")
-#         return "<unknown>"
-#
-#     curse_info = response.json()
-#     data = curse_info["data"]
-#     if len(data) == 1:
-#         return str(data[0]["id"])
-#     elif len(data) == 0:
-#         log.error(f"Could not find by slug {slug}")
-#     else:
-#         log.error(f"Found too many mods ({len(data)}) for slug {slug}")
-#
-#     return "<unknown>"
-#
-#
-# async def get_mods(mods: list[ExternalModInfo], client: AsyncClient) -> list[dict[str, Any]]:
-#     mod_ids = [m.project_id for m in mods]
-#
-#     curse_info = []
-#     for id_chunk in grouper(50, mod_ids):
-#         response = await client.post(
-#             url=CURSE_BASE_URL + "/v1/mods",
-#             headers=get_headers() | {"Content-Type": "application/json"},
-#             json={"modIds": id_chunk},
-#         )
-#         if response.status_code != 200:
-#             log.exception(f"Error getting mods! {response.status_code}")
-#             return []
-#
-#         curse_info.extend(response.json()["data"])
-#
-#     return curse_info
-#
-#
-# async def get_mod_files(mod: ExternalModInfo, client: AsyncClient) -> list[dict[str, Any]]:
-#     params = {
-#         "gameVersionTypeId": CURSE_GAME_VERSION_TYPE_ID,
-#         "modLoaderType": CURSE_FORGE_MODLOADER_ID,
-#     }
-#     index = 0
-#     has_more = True
-#     mod_files: list[dict[str, Any]] = []
-#     while has_more:
-#         response = await client.get(
-#             url=CURSE_BASE_URL + f"/v1/mods/{mod.project_id}/files",
-#             headers=get_headers(),
-#             params=params | {"index": index},
-#         )
-#         if response.status_code != 200:
-#             log.exception(f"Error getting mod files! {response.status_code}")
-#             return mod_files
-#         response_json = response.json()
-#         if "pagnation" not in response_json:
-#             has_more = False
-#         else:
-#             pagnation = response_json["pagnation"]
-#             result_count = pagnation.get("resultCount", 0)
-#             total_count = pagnation.get("totalCount", 0)
-#             page_size = pagnation.get("pageSize", 50)
-#             if total_count < 50 or result_count < page_size or index + page_size >= total_count:
-#                 has_more = False
-#             index += page_size
-#         mod_files.extend(response_json.get("data", []))
-#
-#     return mod_files
+import shutil
 from pathlib import Path
-from typing import Callable, Optional
+from typing import Callable, Optional, List, Tuple, Dict
+from zipfile import ZipFile
 
+from colorama import Fore
+from structlog import get_logger
+
+from gtnh.assembler.downloader import get_asset_version_cache_location
 from gtnh.assembler.generic_assembler import GenericAssembler
-from gtnh.defs import RELEASE_CURSE_DIR, Side
+from gtnh.defs import RELEASE_CURSE_DIR, Side, ModSource, CACHE_DIR, ROOT_DIR
+from gtnh.models.gtnh_config import GTNHConfig
 from gtnh.models.gtnh_release import GTNHRelease
+from gtnh.models.gtnh_version import GTNHVersion
+from gtnh.models.mod_info import GTNHModInfo, ExternalModInfo
 from gtnh.modpack_manager import GTNHModpackManager
 
+from json import dump, dumps, loads
+
+log = get_logger(__name__)
 
 class CurseAssembler(GenericAssembler):
     """
@@ -128,5 +47,125 @@ class CurseAssembler(GenericAssembler):
             global_progress_callback=global_progress_callback,
         )
 
+        self.overrides_folder = Path("overrides")
+        self.manifest_json = Path("manifest.json")
+        self.dependencies_json = self.overrides_folder / "config" / "dependencies.json"
+        self.tempfile = CACHE_DIR / "temp"
+        self.overrides = ROOT_DIR / "overrides.png"
+        self.overrideslash = ROOT_DIR / "overrideslash.png"
+
     def get_archive_path(self, side: Side) -> Path:
         return RELEASE_CURSE_DIR / f"GTNewHorizons-{side}-{self.release.version}.zip"
+
+    def assemble(self, side: Side, verbose: bool = False) -> None:
+        if side not in {Side.CLIENT}:
+            raise Exception("Can only assemble release for CLIENT")
+
+        archive_name: Path = self.get_archive_path(side)
+
+        # deleting any existing archive
+        if archive_name.exists():
+            archive_name.unlink()
+            log.warn(f"Previous archive {Fore.YELLOW}'{archive_name}'{Fore.RESET} deleted")
+
+        log.info(f"Constructing {Fore.YELLOW}{side}{Fore.RESET} archive at {Fore.YELLOW}'{archive_name}'{Fore.RESET}")
+
+        with ZipFile(self.get_archive_path(side), "w") as archive:
+            log.info("Adding config to the archive")
+            self.add_config(side, self.get_config(), archive, verbose=verbose)
+            log.info("Adding manifest.json to the archive")
+            self.generate_meta_data(side, archive)
+            log.info("Adding dependencies.json to the archive")
+            self.generate_json_dep(side, archive)
+            archive.write(self.overrides, arcname="overrides/overrides.png")
+
+            archive.write(self.overrideslash, arcname="overrides/overrideslash.png")
+
+            log.info("Archive created successfully!")
+
+    def add_config(
+        self, side: Side, config: Tuple[GTNHConfig, GTNHVersion], archive: ZipFile, verbose: bool = False
+    ) -> None:
+        modpack_config: GTNHConfig
+        config_version: Optional[GTNHVersion]
+        modpack_config, config_version = config
+
+        config_file: Path = get_asset_version_cache_location(modpack_config, config_version)
+
+        with ZipFile(config_file, "r") as config_zip:
+
+            for item in config_zip.namelist():
+                if item in self.exclusions[side]:
+                    continue
+                with config_zip.open(item) as config_item:
+                    with archive.open(
+                        str(self.overrides_folder) + "/" + item, "w"
+                    ) as target:  # can't use Path for the whole
+                        # path here as it strips leading / but those are used by
+                        # zipfile to know if it's a file or a folder. If used here,
+                        # Path objects will lead to the creation of empty files for
+                        # every folder.
+                        shutil.copyfileobj(config_item, target)
+                        if self.task_progress_callback is not None:
+                            self.task_progress_callback(self.get_progress(), f"adding {item} to the archive")
+
+    def generate_json_dep(self, side:Side, archive:ZipFile):
+        mod_list: List[Tuple[GTNHModInfo | ExternalModInfo, GTNHVersion]] = self.get_mods(side)
+        mod: GTNHModInfo | ExternalModInfo
+        version: GTNHVersion
+        dep_json: List[Dict[str, str]] = []
+        for mod, version in mod_list:
+            if mod.source != ModSource.curse:
+                mod_obj: Dict[str, str] = {
+                    "path":f"mods/{version.filename}",
+                    "url":version.download_url
+                }
+
+                dep_json.append(mod_obj)
+
+        with open(self.tempfile, "w") as temp:
+            dump(dep_json,temp, indent=2)
+
+        archive.write(self.tempfile, arcname=str(self.dependencies_json))
+
+
+
+    def generate_meta_data(self, side:Side, archive:ZipFile):
+        metadata=\
+"""{
+  "minecraft": {
+    "version": "1.7.10",
+    "modLoaders": [
+      {
+        "id": "forge-10.13.4.1614",
+        "primary": true
+      }
+    ]
+  },
+  "manifestType": "minecraftModpack",
+  "manifestVersion": 1,
+  "name": "GT New Horizons",
+  "version": "{0}-1.7.10",
+  "author": "DreamMasterXXL",
+  "files": [],
+  "overrides": "overrides"
+}"""
+        metadata_object = loads(metadata)
+        metadata_object["version"] = metadata_object["version"].format(self.release.version)
+        for mod, version in self.get_mods(side):
+            if mod.source == ModSource.curse:
+                data: Dict[str, int|bool]={
+                    "projectID": mod.project_id,
+                    "fileID": version.browser_download_url.split("/")[-1],  # hacky af but i don't want to go in the
+                                                                            # process of readding them all by hand
+                                                                            # while the data is still stored somewhere
+                                                                            # else in the metadata
+                    "required": True
+                }
+                metadata_object["files"].append(data)
+
+        with open(self.tempfile, "w") as temp:
+            dump(metadata_object, temp, indent=2)
+
+        archive.write(self.tempfile, arcname=str(self.manifest_json))
+
