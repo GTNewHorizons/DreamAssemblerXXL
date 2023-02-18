83c2845bf6a5f6aef3fff8001e8597bb3033f293
Raven Szewczyk <git@eigenraven.me>
2023-02-18 22:54:37 +0000

    Remove the UseShenandoahGC server flag as it doesn't work with GraalVM (#101)

--- server_assets/forge/startserver-java9.bat
+++ server_assets/forge/startserver-java9.bat
@@ -1,2 +1,2 @@
 rem don't forget to accept the EULA or it won't boot
-java -Xms6G -Xmx6G -XX:+UseShenandoahGC -Dfml.readTimeout=180 @java9args.txt nogui
+java -Xms6G -Xmx6G -Dfml.readTimeout=180 @java9args.txt nogui
