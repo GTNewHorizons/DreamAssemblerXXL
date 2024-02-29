d60a380cf6cf7b09f3b0f8f8f28ae77f1c79aca2
Raven Szewczyk <git@eigenraven.me>
2024-02-29 19:46:53 +0000

    Update server java9args to the RFB lwjgl3ify version

--- server_assets/forge/startserver-java9.bat
+++ server_assets/forge/startserver-java9.bat
@@ -1,2 +1,2 @@
 rem don't forget to accept the EULA or it won't boot
-java -Xms6G -Xmx6G -Dfml.readTimeout=180 @java9args.txt nogui
+java -Xms6G -Xmx6G -Dfml.readTimeout=180 @java9args.txt -jar lwjgl3ify-forgePatches.jar nogui
