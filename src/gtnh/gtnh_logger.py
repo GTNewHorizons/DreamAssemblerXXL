1dddfc9ced56f9e8b102698b4b162ee671f9fa4b
Caedis <Caedis@users.noreply.github.com>
2024-03-02 22:08:23 -0600

    force cast

--- src/gtnh/gtnh_logger.py
+++ src/gtnh/gtnh_logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import typing
 
 import structlog
 
@@ -8,4 +9,4 @@ def get_logger(name: str) -> structlog.BoundLogger:
     if not structlog.is_configured():
         LOG_LEVEL = getattr(logging, os.environ.get("LOG_LEVEL", "INFO").upper())
         structlog.configure(wrapper_class=structlog.make_filtering_bound_logger(LOG_LEVEL))
-    return structlog.get_logger(name)
+    return typing.cast(structlog.BoundLogger ,structlog.get_logger(name))
