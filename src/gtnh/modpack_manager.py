f21ad78021c58ce225ceccab54cc358c3ed27ece
Caedis <Caedis@users.noreply.github.com>
2024-03-08 11:05:32 -0600

    init pinned and local exclusions if file doesnt exist

--- src/gtnh/modpack_manager.py
+++ src/gtnh/modpack_manager.py
@@ -1015,10 +1015,14 @@ class GTNHModpackManager:
         if os.path.exists(self.local_exclusions_path):
             with open(self.local_exclusions_path, "r") as f:
                 local_exclusions = f.read().splitlines()
+        else:
+            local_exclusions = []
 
         if os.path.exists(self.inplace_pinned_mods):
             with open(self.inplace_pinned_mods, "r") as f:
                 pinned_mods = f.read().splitlines()
+        else:
+            pinned_mods = []
 
         # cache all active mods
         active_mods = glob.glob("*.jar", root_dir=mods_dir) + glob.glob("1.7.10/*.jar", root_dir=mods_dir)
@@ -1045,7 +1049,7 @@ class GTNHModpackManager:
             mod = mod_ver[0]
             version = mod_ver[1]
 
-            if mod.name in pinned_mods if pinned_mods else []:
+            if mod.name in pinned_mods:
                 log.debug(f"{Fore.YELLOW}{mod.name}{Fore.RESET} is pinned, skipping")
                 continue
 
@@ -1054,7 +1058,7 @@ class GTNHModpackManager:
                 continue
 
             # ignore mods that are excluded in the target mods directory
-            if mod.name in local_exclusions if local_exclusions else []:
+            if mod.name in local_exclusions:
                 log.debug(f"{Fore.YELLOW}{mod.name}{Fore.RESET} is locally excluded, skipping")
                 continue
 
