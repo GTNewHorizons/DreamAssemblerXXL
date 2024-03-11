a10d69687c6f992a40778c4fb1ff73ec2e8923db
boubou19 <miisterunknown@gmail.com>
2024-03-11 13:41:01 +0100

    fix multi_poly clientside archives

--- src/gtnh/assembler/multi_poly.py
+++ src/gtnh/assembler/multi_poly.py
@@ -122,7 +122,8 @@ class MMCAssembler(GenericAssembler):
         await GenericAssembler.assemble(self, side, verbose)
 
         with ZipFile(self.get_archive_path(side), "a", compression=ZIP_DEFLATED) as archive:
-            self.add_localisation_files(archive, str(self.mmc_modpack_files))
+            self.add_localisation_files(archive, str(self.mmc_modpack_files.as_posix()))  # otherwise file check fails
+            # on windows
 
         self.add_mmc_meta_data(side)
 
