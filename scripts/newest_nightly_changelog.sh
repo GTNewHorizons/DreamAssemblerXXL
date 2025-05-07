dffc89c477f3e367316f9f8fae87e04893558101
Caedis <Caedis@users.noreply.github.com>
2025-05-07 00:20:39 -0500

    Remove extension from output

--- scripts/newest_nightly_changelog.sh
+++ scripts/newest_nightly_changelog.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find "$1" -maxdepth 1 -name '*.md' -printf '%P\n' | sort -rV | head -n1
+find "$1" -maxdepth 1 -name '*.md' -printf '%P\n' | sort -rV | head -n1 | xargs -I _ basename _ .md
