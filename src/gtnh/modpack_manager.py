7aa481d5d266f4d6f6543bf9fa7af7be90590be7
Caedis <Caedis@users.noreply.github.com>
2025-07-18 03:47:33 -0500

    Dont update mods that have a side of NONE

--- src/gtnh/modpack_manager.py
+++ src/gtnh/modpack_manager.py
@@ -319,6 +319,11 @@ class GTNHModpackManager:
     async def update_versions_from_repo(
         self, asset: Versionable, repo: AttributeDict, for_translation: bool = False, releaseVersion: str | None = None
     ) -> bool:
+        # dont update mods with a side of NONE
+        if isinstance(asset, GTNHModInfo):
+            if asset.side == Side.NONE:
+                return False
+
         releases = [AttributeDict(r) async for r in self.gh.getiter(repo_releases_uri(self.org, repo.name))]
         if for_translation:
             releases = [r for r in releases if r.tag_name.endswith("-latest")]
