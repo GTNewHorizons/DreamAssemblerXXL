dcb59dc95ebb2b94848eaa693a31e8e5cee75c75
IanDresarie <franzje@quantentunnel.de>
2026-04-12 15:51:30 +0200

    Update startserver-java9.bat with silent confirm

--- server_assets/forge/startserver-java9.bat
+++ server_assets/forge/startserver-java9.bat
@@ -1,3 +1,4 @@
 rem don't forget to accept the EULA or it won't boot
+rem add this argument behind the other "-Dfml..." to silently migrate your world during startup, a backup will be created: Dfml.queryResult=confirm
 java -Xms6G -Xmx6G -Dfml.readTimeout=180 @java9args.txt -jar lwjgl3ify-forgePatches.jar nogui
 pause
