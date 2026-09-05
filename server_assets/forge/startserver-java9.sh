cd280ff49e119b065de882d2103254fbac640444
PLASMAchicken <plasmachickenmc@gmail.com>
2026-09-05 21:14:11 +0200

    force language to english
    Same as #270 but for linux startup to fix https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues/26622

--- server_assets/forge/startserver-java9.sh
+++ server_assets/forge/startserver-java9.sh
@@ -4,7 +4,7 @@
 # add this argument behind the other "-Dfml..." to silently migrate your world during startup, a backup will be created: -Dfml.queryResult=confirm
 while true
 do
-   java -Xms6G -Xmx6G -Dfml.readTimeout=180 @java9args.txt -jar lwjgl3ify-forgePatches.jar nogui
+   java -Xms6G -Xmx6G -Dfml.readTimeout=180 -Duser.language=en @java9args.txt -jar lwjgl3ify-forgePatches.jar nogui
     echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
     echo "Rebooting in:"
     for i in 12 11 10 9 8 7 6 5 4 3 2 1
